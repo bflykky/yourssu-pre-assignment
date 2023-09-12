dd45ed95126cb813262ee3dfd3b0be141b41972e
김강연 <kkyxw@naver.com>
2023-09-12 22:18:40 +0900

    init: 간단한 블로그 서버 프로젝트 초기 세팅
    프로젝트의 세팅은 사전과제 요구사항을 준수하여 설정하였다.
    - Build Tool: Gradle
    - Language: Kotlin
    - Language level: Java 11
    - Spring version: 2.7.15
    
    초기에 추가한 의존성 목록은 다음과 같다.
    - Spring Web
    - Spring Data JPA
    - Spring Boot DevTools
    - Spring Security
    - Validation
    - MySQL Driver
    - Lombok

---
+++ src/main/kotlin/com/yourssu/preassignment/PreAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.yourssu.preassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PreAssignmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<PreAssignmentApplication>(*args)
+}
