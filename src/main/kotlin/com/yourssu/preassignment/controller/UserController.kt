1cd20dcf24667d39eb92ee1184a25e6598321437
김강연 <kkyxw@naver.com>
2023-09-16 19:12:24 +0900

    feat: 회원탈퇴 API 구현
    - quit(deleteRequestDto: DeleteRequestDto)
      회원 탈퇴 시, 회원이 작성한 게시글과 댓글을 모두 삭제하도록 로직을 구현하였다.
      이때, 회원이 작성한 게시글에 다른 회원이 댓글을 작성한 경우, CascadeType.ALL
      옵션이 걸려 있어도 외래키 제약조건에 의해 삭제가 제대로 되지 않는 현상이 발생하였다.
      따라서, 삭제하려는 회원의 게시글을 먼저 삭제해준 후, 회원을 삭제해주도록 코드를 구현
      하였다.

--- src/main/kotlin/com/yourssu/preassignment/controller/UserController.kt
+++ src/main/kotlin/com/yourssu/preassignment/controller/UserController.kt
@@ -1,8 +1,10 @@
 package com.yourssu.preassignment.controller
 
+import com.yourssu.preassignment.request.DeleteRequestDto
 import com.yourssu.preassignment.request.UserJoinRequestDto
 import com.yourssu.preassignment.response.UserJoinResponse
 import com.yourssu.preassignment.service.UserService
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -14,6 +16,11 @@ class UserController(
     @PostMapping(value = ["/users"])
     fun join(@RequestBody userJoinRequestDto: UserJoinRequestDto): UserJoinResponse {
         return userService.join(userJoinRequestDto);
+    }
+
+    @DeleteMapping(value = ["/users"])
+    fun quit(@RequestBody deleteRequestDto: DeleteRequestDto) {
+        return userService.quit(deleteRequestDto)
     }
 
 }
--- src/main/kotlin/com/yourssu/preassignment/service/UserService.kt
+++ src/main/kotlin/com/yourssu/preassignment/service/UserService.kt
@@ -1,7 +1,9 @@
 package com.yourssu.preassignment.service
 
 import com.yourssu.preassignment.entity.User
+import com.yourssu.preassignment.repository.ArticleRepository
 import com.yourssu.preassignment.repository.UserRepository
+import com.yourssu.preassignment.request.DeleteRequestDto
 import com.yourssu.preassignment.request.UserJoinRequestDto
 import com.yourssu.preassignment.response.UserJoinResponse
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -11,6 +13,7 @@ import java.time.LocalDateTime
 @Service
 class UserService(
     private val userRepository: UserRepository,
+    private val articleRepository: ArticleRepository,
     private val passwordEncoder: PasswordEncoder
 ) {
     fun join(userJoinRequestDto: UserJoinRequestDto): UserJoinResponse {
@@ -27,5 +30,20 @@ class UserService(
         userRepository.save(user)
 
         return UserJoinResponse(email = user.email, username = user.username)
+    }
+
+    fun quit(deleteRequestDto: DeleteRequestDto) {
+        val user: User = userRepository.findByEmail(deleteRequestDto.email).orElse(null)
+            ?: throw RuntimeException("해당 이메일을 가진 회원이 존재하지 않습니다.")
+
+        if (passwordEncoder.matches(deleteRequestDto.password, user.password) == false) {
+            throw RuntimeException("비밀번호가 일치하지 않습니다. 다시 시도해 주세요.")
+        }
+
+        // 해당 회원이 작성한 게시글 전부 삭제
+        for (article in user.articleList) {
+            articleRepository.delete(article)
+        }
+        userRepository.delete(user)
     }
 }
